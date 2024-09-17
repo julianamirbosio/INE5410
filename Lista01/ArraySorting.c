dfc23d2c6610553932cc88a681276e7c23bfcdc6
leonardo-sousa <leonardo.sousa@aprovatotal.com.br>
2024-09-16 21:39:28 -0300

    MinMax e ArraySorting

--- Lista01/ArraySorting.c
+++ Lista01/ArraySorting.c
@@ -1,20 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Bubblesort - complexidade O(n²)
-void bubbleSort(int arr[], int n) {
-    int i, j, temp;
-    for (i = 0; i < n-1; i++) {
-        for (j = 0; j < n-i-1; j++) {
-            if (arr[j] > arr[j+1]) {
-                temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
-    }
-}
-
 //Lê valores do array via terminal
 void inputArray(int * arr, int size) {
     for (int i = 0; i < size; i++) {
